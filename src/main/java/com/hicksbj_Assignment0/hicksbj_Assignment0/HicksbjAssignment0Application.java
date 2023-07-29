1a373e94cc1b7e3ab1de9b1467e05c88ee4852a0
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2023-07-29 13:58:31 -0400

    Committing original attempt changes

---
+++ src/main/java/com/hicksbj_Assignment0/hicksbj_Assignment0/HicksbjAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.hicksbj_Assignment0.hicksbj_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HicksbjAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HicksbjAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hicksbj_Assignment0/hicksbj_Assignment0/HicksbjAssignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hicksbj_Assignment0.hicksbj_Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HicksbjAssignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
